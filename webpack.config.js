4f0fd3e931e976f5f65478c973e1b519a4d18921
skoob13 <georgiy@ridgebox.com>
2018-04-20 18:37:11 +0300

    update build

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+/* global __dirname, require, module*/
+
+const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const nodeExternals = require('webpack-node-externals');
+const path = require('path');
+const env = require('yargs').argv.env; // use --env with webpack 2
+const pkg = require('./package.json');
+
+let libraryName = pkg.name;
+
+let outputFile, mode;
+
+if (env === 'build') {
+  outputFile = libraryName + '.min.js';
+  mode = 'production';
+} else {
+  outputFile = libraryName + '.js';
+  mode = 'development';
+}
+
+const config = {
+  mode,
+  entry: __dirname + '/src/index.js',
+  devtool: 'source-map',
+  output: {
+    path: __dirname + '/lib',
+    filename: outputFile,
+    library: libraryName,
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  module: {
+    rules: [
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: 'babel-loader',
+        exclude: /(node_modules|bower_components)/
+      }
+    ]
+  },
+  resolve: {
+    modules: [path.resolve('./node_modules'), path.resolve('./src')],
+    extensions: ['.json', '.js']
+  },
+  externals: [nodeExternals()],
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        uglifyOptions: {
+          compress: false,
+          ecma: 6,
+          mangle: true
+        },
+        sourceMap: true
+      })
+    ]
+  },
+};
+
+module.exports = config;
